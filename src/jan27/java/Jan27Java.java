a68f076b44ed95e63e498bcdf0a5e48396bc1a0b
Max Matthews <mattmadi@shernet.sheridancollege.ca>
2023-01-27 11:35:47 -0500

    Comment

---
+++ src/jan27/java/Jan27Java.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package jan27.java;
+
+/**
+ *
+ * @author Max Matthews
+ */
+public class Jan27Java {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
